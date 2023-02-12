5895262b6cbeb5d8615e08d4bfb5ab98848cf2b5
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-12 12:56:23 +0300

    starter

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
