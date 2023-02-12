995442e62fa4c7f60d7dd762fba72ec49b0a8db3
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-12 12:58:24 +0300

    scss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
